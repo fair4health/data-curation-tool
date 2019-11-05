73247c59bc3257a6109fc23e790ae714981fa2a6
A. Anil SINACI <anil@srdc.com.tr>
2019-11-05 10:56:46 +0300

    test webhook

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -3,10 +3,10 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator'
+  import {Component, Prop, Vue} from 'vue-property-decorator'
 
-@Component
-export default class Message extends Vue {
-  @Prop() private msg!: string;
-}
+  @Component
+  export default class Message extends Vue {
+    @Prop() private msg!: string;
+  }
 </script>
