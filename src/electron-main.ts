348df2688ae7f3254f66c669bb88feeefacc1b01
Alper Teoman <teooalp@hotmail.com>
2020-06-08 10:03:48 +0300

    fix(app): Quit the app completely on Darwin - window closing behavior

--- src/electron-main.ts
+++ src/electron-main.ts
@@ -122,11 +122,8 @@ function createBgWindow (id: number): BrowserWindow {
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+  // On macOS, quits the app as Cmd + Q does
+  app.quit()
 })
 
 app.on('activate', () => {
